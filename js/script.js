96a72738f8334c5001fc2277a8fd76d6b5923a43
macin <ernesto.dev24m@gmail.com>
2024-08-08 12:34:25 -0600

    Add new onAddItemBtn function to reuse code

--- js/script.js
+++ js/script.js
@@ -14,9 +14,53 @@ const emptyCart = document.querySelector(".cart__empty");
 let cartForItems = [];
 
 
+const onAddItemBtn = ({cardBtn, logoBtn, pBtn, objItem}) => {
+
+    cardBtn.removeChild(logoBtn);
+
+    const plusBtn = document.createElement("div");
+    const minParent = document.createElement("div");
+    const minBtn = document.createElement("div");
+    const plusParent = document.createElement("div");
+    const validate = document.getElementById(`btn-id.${objItem.id}`);
+
+    if (!validate) {
+
+        // Add classes attributes
+        cardBtn.classList.add('item__button-click')
+        plusParent.classList.add("parent-btns");
+        minParent.classList.add("parent-btns");
+        plusBtn.classList.add("increment-btn");
+        minBtn.classList.add("decrement-btn");
+
+        cardBtn.id = `btn-id.${objItem.id}`
+
+        // Append childs elements
+        plusParent.appendChild(plusBtn);
+        minParent.appendChild(minBtn);
+        cardBtn.appendChild(minParent)
+        cardBtn.appendChild(pBtn)
+        cardBtn.appendChild(plusParent);
+
+        plusBtn.style.backgroundImage = 'url("./assets/images/icon-increment-quantity.svg")';
+        minBtn.style.backgroundImage = 'url("./assets/images/icon-decrement-quantity.svg")'
+        pBtn.innerText = 1;
+
+        // Add click event
+        plusParent.onclick = () => {
+            onFilterItems(objItem);
+        };
+
+        onFilterItems(objItem);
+
+        cardBtn.onclick = false;
+
+    }
+}
+
 
 const onCreateElement = ({ image, name, category, price }) => {
-    
+
     const cardContainer = document.createElement("div");
     const cardImage = document.createElement("div");
     const cardBtn = document.createElement("button");
@@ -28,7 +72,7 @@ const onCreateElement = ({ image, name, category, price }) => {
     const articleSpan = document.createElement("span");
 
     // Condition to change background
-    if(window.innerWidth >= 1440){
+    if (window.innerWidth >= 1440) {
         cardImage.style.backgroundImage = `url(${image.desktop})`;
         cardImage.classList.add("desktop");
     } else {
@@ -61,52 +105,8 @@ const onCreateElement = ({ image, name, category, price }) => {
         thumbnail: image.thumbnail
     }
 
-    // Remeber add an event listener over
-    cardBtn.onclick = (e) => {
-        cardBtn.removeChild(logoBtn);
-
-        const plusBtn = document.createElement("div");
-        const minParent = document.createElement("div");
-        const minBtn = document.createElement("div"); 
-        const plusParent = document.createElement("div");
-        const validate = document.getElementById(`btn-id.${idItem}`);
-
-        if(!validate){
-            
-            // Add classes attributes
-            cardBtn.classList.add('item__button-click')
-            plusParent.classList.add("parent-btns");
-            minParent.classList.add("parent-btns");
-            plusBtn.classList.add("increment-btn");
-            minBtn.classList.add("decrement-btn");
-
-            cardBtn.id = `btn-id.${idItem}`
-            
-            // Append childs elements
-            plusParent.appendChild(plusBtn);
-            minParent.appendChild(minBtn);
-            cardBtn.appendChild(minParent)
-            cardBtn.appendChild(pBtn)
-            cardBtn.appendChild(plusParent);
-            
-            
-            plusBtn.style.backgroundImage = 'url("./assets/images/icon-increment-quantity.svg")';
-            minBtn.style.backgroundImage = 'url("./assets/images/icon-decrement-quantity.svg")'
-            pBtn.innerText = 1;
-
-            // Add click event
-            plusParent.onclick = () => {
-                onFilterItems(objItem);
-            };
-
-            
-            onFilterItems(objItem);
-
-            cardBtn.onclick = false;
-
-        }
-
-        
+    cardBtn.onclick = () => {
+        onAddItemBtn({cardBtn, logoBtn, pBtn, objItem})
     };
 
     // Append HTMl elements
@@ -120,12 +120,12 @@ const onCreateElement = ({ image, name, category, price }) => {
     cardContainer.appendChild(article);
 
     return cardContainer;
-    
+
 
 }
 
 // Fetch - Format JSON
-async function onGetData () {
+async function onGetData() {
 
     try {
         const response = await fetch("../data.json");
@@ -140,7 +140,7 @@ async function onGetData () {
 const onDisplay = async () => {
     const data = await onGetData();
 
-    for(let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         cardContent.appendChild(onCreateElement(data[i]));
     }
 }
@@ -151,7 +151,7 @@ const onHandleImageSize = async () => {
 
     const data = await onGetData();
 
-    if(window.innerWidth >= 1440 && imgSize.classList.contains("mobile")){
+    if (window.innerWidth >= 1440 && imgSize.classList.contains("mobile")) {
 
         data.forEach(({ image }, i) => {
             arryHTML[i].style.backgroundImage = `url(${image.desktop})`;
@@ -160,7 +160,7 @@ const onHandleImageSize = async () => {
         imgSize.classList.remove("mobile");
         imgSize.classList.add("desktop");
 
-    } else if(window.innerWidth <= 1439 && imgSize.classList.contains("desktop")){
+    } else if (window.innerWidth <= 1439 && imgSize.classList.contains("desktop")) {
 
         data.forEach(({ image }, i) => {
             arryHTML[i].style.backgroundImage = `url(${image.mobile})`;
@@ -171,27 +171,27 @@ const onHandleImageSize = async () => {
     }
 }
 
-const onFilterItems = ({id, name, price, thumbnail}) => {
+const onFilterItems = ({ id, name, price, thumbnail }) => {
 
     const newItem = {
-        id, 
-        name, 
-        price, 
-        thumbnail, 
+        id,
+        name,
+        price,
+        thumbnail,
         quantity: 1,
         total: price
     }
 
-    if(cartForItems.length === 0){
+    if (cartForItems.length === 0) {
         cartForItems.push(newItem);
     } else {
         const existItem = cartForItems.some(item => item.id === id)
-        
-        if(!existItem){
+
+        if (!existItem) {
             cartForItems = [newItem, ...cartForItems];
         } else {
             cartForItems = cartForItems.map(item => {
-                if(item.id === id){
+                if (item.id === id) {
                     item.quantity++;
                     item.total += item.price;
                 }
@@ -207,44 +207,44 @@ const onFilterItems = ({id, name, price, thumbnail}) => {
 
 const onDisplayConfirm = () => {
 
-        // Total elements
-        const totalText = document.createElement("p");
+    // Total elements
+    const totalText = document.createElement("p");
 
-        // Confirm elements
-        const confirmText = document.createElement("div");
-        const confirmIcon = document.createElement("div");
-        const confirmIconText = document.createElement("p");
-        const confirmBtn = document.createElement("button");
-    
-        // Display Total section 
-        totalValue.classList.add("item__total-text");
-    
-        // Display Confirm section
-        confirmText.classList.add("item__confirm-text");
-        confirmIcon.classList.add("item__confirm-icon");
-        confirmBtn.classList.add("item__confirm-btn");
-        totalContainer.classList.add("item__active");
-    
-        // Add values
-        totalText.innerText = "Order Total";
-        confirmIconText.innerHTML = "p>This is a <b>Carbon-neutral</b> delivery</p>";
-        confirmBtn.innerText = "Confirm Order";
-    
-        // Append elements
-        totalContainer.appendChild(totalText);
-        totalContainer.appendChild(totalValue);
-    
-        confirmText.appendChild(confirmIcon);
-        confirmText.appendChild(confirmIconText);
-        confirmContainer.appendChild(confirmText);
-        confirmContainer.appendChild(confirmBtn);
-    
-        cartFullyContainer.appendChild(totalContainer);
-        cartFullyContainer.appendChild(confirmContainer);
-    
-        totalContainer.style.display = "flex";
-        confirmContainer.style.display = "flex";
-    
+    // Confirm elements
+    const confirmText = document.createElement("div");
+    const confirmIcon = document.createElement("div");
+    const confirmIconText = document.createElement("p");
+    const confirmBtn = document.createElement("button");
+
+    // Display Total section 
+    totalValue.classList.add("item__total-text");
+
+    // Display Confirm section
+    confirmText.classList.add("item__confirm-text");
+    confirmIcon.classList.add("item__confirm-icon");
+    confirmBtn.classList.add("item__confirm-btn");
+    totalContainer.classList.add("item__active");
+
+    // Add values
+    totalText.innerText = "Order Total";
+    confirmIconText.innerHTML = "p>This is a <b>Carbon-neutral</b> delivery</p>";
+    confirmBtn.innerText = "Confirm Order";
+
+    // Append elements
+    totalContainer.appendChild(totalText);
+    totalContainer.appendChild(totalValue);
+
+    confirmText.appendChild(confirmIcon);
+    confirmText.appendChild(confirmIconText);
+    confirmContainer.appendChild(confirmText);
+    confirmContainer.appendChild(confirmBtn);
+
+    cartFullyContainer.appendChild(totalContainer);
+    cartFullyContainer.appendChild(confirmContainer);
+
+    totalContainer.style.display = "flex";
+    confirmContainer.style.display = "flex";
+
 }
 
 const onDisplayItem = (items) => {
@@ -252,13 +252,13 @@ const onDisplayItem = (items) => {
     let totalItems = 0;
     let quantity = 0;
 
-    if(!totalContainer.classList.contains("item__active")){
+    if (!totalContainer.classList.contains("item__active")) {
         emptyCart.style.display = "none"
         cart.style.height = "600px"
         onDisplayConfirm();
     };
 
-    while(itemsContainer.lastChild){
+    while (itemsContainer.lastChild) {
         itemsContainer.removeChild(itemsContainer.lastChild);
     }
 
@@ -274,7 +274,7 @@ const onDisplayItem = (items) => {
         const itemPrice = document.createElement("p");
         const itemTotal = document.createElement("p");
         const itemRemove = document.createElement("button");
-    
+
         // Add classes to each Node elements
         itemName.classList.add("item__cart-dt");
         itemQuantity.classList.add("item__cart-dq");
@@ -288,7 +288,7 @@ const onDisplayItem = (items) => {
         itemQuantity.innerText = `${item.quantity}x`;
         itemPrice.innerText = `@${Number(item.price).toFixed(2)}`;
         itemTotal.innerText = `$${Number(item.total).toFixed(2)}`;
-    
+
         // Append elements to each other
         list.appendChild(itemName);
         list.appendChild(itemQuantity);
@@ -299,10 +299,10 @@ const onDisplayItem = (items) => {
         itemsContainer.appendChild(list);
 
     })
-    
+
     cartNumber.innerText = `(${quantity})`;
     totalValue.innerText = `$${totalItems.toFixed(2)}`;
-    
+
 }
 
 window.addEventListener('resize', onHandleImageSize);
